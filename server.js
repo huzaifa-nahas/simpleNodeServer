67dc98728c3af9d632145640af7f7cd6b2c0b48f
Huzaifa <halnahas@gmail.com>
2020-12-28 12:37:08 -0500

    Simple Node Server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+const config = require('./config');
+
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
+// app.use(bodyParser.json());
+app.use(cors());
+
+// api routes
+app.get("/api/request", (req, res, next) => {
+    if (req.query.number == 404)
+        return res.status(404).send({
+            message: 'This is an error!'
+        });
+    else
+        res.json({
+            message: "Success"
+        });
+});
+
+// start server
+const port = config.env === 'production' ? 80 : config.port;
+const server = app.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
