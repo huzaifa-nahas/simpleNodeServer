888428d00bb0da44c2157f4999cdffefa13def52
Huzaifa <halnahas@gmail.com>
2020-12-29 01:50:35 -0500

    cors

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.use(cors());
 
 app.use(favicon(__dirname + '/favicon.ico'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // api routes
 app.get("/api/request", (req, res, next) => {
     if (req.query.number == 404)
